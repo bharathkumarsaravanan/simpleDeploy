dde0d1d256fabf003aa0a87566345e27cb13a771
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-02-12 00:57:40 +0530

    hope

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-axios.defaults.baseURL = "http://43.205.114.56/"
+axios.defaults.baseURL = "http://43.205.114.56"
 
 function App(){
   return(
